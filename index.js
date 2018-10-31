c9ac38f0a87259321116465a0d51363bedfe8133
guillaume charbonnier <gcharbonnier>
2018-10-31 09:32:22 +0100

    add local config file - use Parse server v3

--- index.js
+++ index.js
@@ -4,22 +4,38 @@
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
-
+var ParseDashboard = require('parse-dashboard');
+var localConfig = require('./localConfig.json'); //save default(when env vars are not set) config values (file ignored by git)
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
 if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 
+
+
+
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: databaseUri || localConfig.databaseURI,
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  appId: process.env.APP_ID || localConfig.appId,
+  masterKey: process.env.MASTER_KEY || localConfig.masterKey,
+  serverURL: process.env.SERVER_URL || localConfig.serverURL,
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
+});
+
+var options = { allowInsecureHTTP: true };
+var dashboard = new ParseDashboard({
+  "apps": [
+    {
+      "serverURL": process.env.SERVER_URL || localConfig.serverURL,
+      "appId": process.env.APP_ID || localConfig.appId,
+      "masterKey": process.env.MASTER_KEY || localConfig.masterKey,
+      "appName": localConfig.appName,
+    }
+  ],options
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
@@ -33,10 +49,11 @@ app.use('/public', express.static(path.join(__dirname, '/public')));
 // Serve the Parse API on the /parse URL prefix
 var mountPath = process.env.PARSE_MOUNT || '/parse';
 app.use(mountPath, api);
+app.use('/dashboard', dashboard);
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
-  res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
+  res.status(200).send('Parse-Server for demonstrating '+ (process.env.APP_ID || localConfig.appId));
 });
 
 // There will be a test page available on the /test path of your server url

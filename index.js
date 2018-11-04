663696af337484a830b4e8e07cd7af2b814c83f3
guillaume charbonnier <gcharbonnier>
2018-11-04 23:39:04 +0100

    add REST key, mailgun config

--- index.js
+++ index.js
@@ -21,8 +21,38 @@ var api = new ParseServer({
   appId: process.env.APP_ID || localConfig.appId,
   masterKey: process.env.MASTER_KEY || localConfig.masterKey,
   serverURL: process.env.SERVER_URL || localConfig.serverURL,
+  clientKey : process.env.CLIENT_KEY || localConfig.clientKey,
+  restAPIKey : process.env.REST_KEY || localConfig.restAPIKey,
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+  },
+  verifyUserEmails: true,
+  emailVerifyTokenValidityDuration: 24 * 60 * 60,
+  preventLoginWithUnverifiedEmail: false,
+  publicServerURL: 'https://qtbaassample.herokuapp.com/parse',
+  appName: 'BaaSSample',
+  emailAdapter: {
+    module: '@parse/simple-mailgun-adapter',
+    options: {
+      // The address that your emails come from
+      fromAddress: process.env.MAILGUN_FROMADDRESS || localConfig.mailgunFromAddress,
+      // Your domain from mailgun.com
+      domain: process.env.MAILGUN_DOMAIN || localConfig.mailgunDomain,
+      // Your API key from mailgun.com
+      apiKey: process.env.MAILGUN_APIKEY || localConfig.mailgunApiKey,
+    }
+    },
+    accountLockout: {
+    duration: 5, // duration policy setting determines the number of minutes that a locked-out account remains locked out before automatically becoming unlocked. Set it to a value greater than 0 and less than 100000.
+    threshold: 3, // threshold policy setting determines the number of failed sign-in attempts that will cause a user account to be locked. Set it to an integer value greater than 0 and less than 1000.
+  },
+  passwordPolicy: {
+    validatorPattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/, // enforce password with at least 8 char with at least 1 lower case, 1 upper case and 1 digit
+    validatorCallback: (password) => { return validatePassword(password) },
+    doNotAllowUsername: true, // optional setting to disallow username in passwords
+    maxPasswordAge: 90, // optional setting in days for password expiry. Login fails if user does not reset the password within this period after signup/last reset.
+    maxPasswordHistory: 5, // optional setting to prevent reuse of previous n passwords. Maximum value that can be specified is 20. Not specifying it or specifying 0 will not enforce history.
+    resetTokenValidityDuration: 24*60*60, // expire after 24 hours
   }
 });
 

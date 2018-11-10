11883f015ad12e453778ee0fe85eedfac71fd2e6
guillaume charbonnier <gcharbonnier>
2018-11-10 20:21:52 +0100

    remove empty iOS push notif settings

--- index.js
+++ index.js
@@ -27,12 +27,6 @@ var api = new ParseServer({
     android: {
       apiKey: process.env.FCM_API_KEY || localConfig.fcmApiKey,
     },
-    ios: {
-      pfx: '/file/path/to/XXX.p12',
-      passphrase: '', // optional password to your p12/PFX
-      bundleId: '',
-      production: false,
-    }
   },
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
